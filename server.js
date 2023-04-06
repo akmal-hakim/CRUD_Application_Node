d85c740c16cbe3e52cdf9d22893a0bd6a7c5e801
Dude that Program <102004927+akmal-hakim@users.noreply.github.com>
2023-04-06 14:04:34 +0800

    Update Latest

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const bodyparser = require("body-parser");
+const express = require('express'); 
+const dotenv = require('dotenv'); // Allows to seperate your secret from your source code
+const morgan = require('morgan');// Allows us to log a request on a console whenever we make a request
+const bodyparser = require("body-parser"); //
 const path = require('path');
 
 const connectDB = require('./server/database/connection');
@@ -22,6 +22,8 @@ app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
 app.set("view engine", "ejs")
+
+// must include const path. __dirname is the CRUD_APPLICATION_NODE (folder name)
 //app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
@@ -33,3 +35,50 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+
+/*
+	Old Ways to load page PART 1
+	app.get('/',(req,res)=>{
+		res.send("Crud Application");
+	})
+
+	app.listen(3000, ()=> { console.log(`Server is running on http://localhost:${3000}`)});
+/*
+
+/*
+	Old Ways to load page PART 2
+	const PORT = process.env.PORT || 8080 
+	//8080 is the default value
+	app.get('/',(req,res)=>{
+		res.send("Crud Application");
+	})
+
+	app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+/*
+
+/*
+	Old Ways to load page PART 3
+	//Must create a config.env file and set its PORT
+	const dotenv = require('dotenv');
+	
+	dotenv.config( {path:'config.env'})
+	const PORT = process.env.PORT || 8080 
+	//8080 is the default value
+
+	app.get('/',(req,res)=>{
+		res.send("Crud Application");
+	})
+
+	app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+/*
+
+/*
+	PART 4
+	// Once already declare EJS - Embended Javascript 
+	app.set("view engine","ejs")
+
+	app.get('/',(req,res)=>{
+		res.render('index'); //respond render index
+	})
+	
+*/
